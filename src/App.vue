2d80c252b0fac5abdbf47445ebe583058e1d429a
odan <odan3240@gmail.com>
2019-02-11 03:25:28 +0900

    impl personal sign

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,8 @@ export default class App extends Vue {
     console.log('poyo1')
     console.log(await this.$signer.connect())
     const message = '0x879a053d4800c6354e76c7985a865d2922c82fb5b3f4577b2fe08b998954f2e0'
-    console.log(await this.$signer.sign(message))
+    // console.log(await this.$signer.sign(message))
+    console.log(await this.$signer.personalSign('poyo'))
     console.log('poyo2')
   }
 }
--- src/lib/signer.ts
+++ src/lib/signer.ts
@@ -1,10 +1,39 @@
 import Web3 from 'web3'
-import { Provider } from 'web3/providers'
+import { Provider, JsonRPCResponse } from 'web3/providers'
 
 export default class Signer {
   private web3: Web3;
   constructor (provider: Provider) {
     this.web3 = new Web3(provider)
+  }
+
+  private send (method: string, params: any[]): Promise<JsonRPCResponse> {
+    return new Promise<JsonRPCResponse>((resolve, reject) => {
+      this.provider.send({
+        method,
+        params,
+        id: 0,
+        jsonrpc: '2.0'
+      }, (error: (null | Error), val?: JsonRPCResponse) => {
+        if (error) {
+          reject(error)
+          return
+        }
+        if (!val) {
+          reject(new Error())
+          return
+        }
+        if (val.error) {
+          reject(val.error)
+        }
+        resolve(val)
+      })
+    })
+  }
+
+  public async poyo (message: string): Promise<void> {
+    const accounts = await this.provider.enable()
+    console.log(await this.send('eth_sign', [accounts[0], message]))
   }
 
   public get provider () {
@@ -22,5 +51,11 @@ export default class Signer {
     const accounts = await this.connect()
     const sign = await this.web3.eth.sign(message, accounts[0])
     return sign
+  }
+
+  public async personalSign (message: string): Promise<string> {
+    const accounts = await this.connect()
+    const result = await this.send('personal_sign', [message, accounts[0]])
+    return result.result
   }
 }
